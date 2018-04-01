beca7c78d6bef11a5994b6811b72d71985047608
徐登伟 <xudengwei1991@gmail.com>
2018-04-01 15:42:34 +0800

    delete some redundancy code and package

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,8 +13,10 @@ module.exports = {
     },
     module: {
         rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            }
         ]
     }
 };
--- webpack.pro.js
+++ webpack.pro.js
@@ -13,20 +13,10 @@ module.exports = {
     },
     module: {
         rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
             {
                 test: /\.tsx?$/, 
                 loader: "ts-loader"
             },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true,
-                    }
-                }]
-            }
         ]
     }
 };
