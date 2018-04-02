c134381e1368ec308838b827f0e809d80ffd7ef6
徐登伟 <xudengwei1991@gmail.com>
2018-04-02 22:07:27 +0800

    modify file of 'webpack.dev.js'

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,8 +11,11 @@ var commonConfig = require('./webpack.common.js');
 
 
 module.exports = webpackMerge(commonConfig, {
+    devServer: {
+        contentBase: './dist'
+    },
     output: {
-        // path: helpers.root('dist'), // 虽然我们告诉Webpack把输出包放到dist目录，但实际上开发服务器把这些包都放在了内存里，而不会把它们写到硬盘中。 所以在dist目录下是找不到任何文件的(至少现在这个开发环境下构建时没有)
+        // 虽然我们告诉Webpack把输出包放到dist目录，但实际上开发服务器把这些包都放在了内存里，而不会把它们写到硬盘中。 所以在dist目录下是找不到任何文件的(至少现在这个开发环境下构建时没有)
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
         chunkFilename: '[id].chunk.js'
@@ -33,10 +36,5 @@ module.exports = webpackMerge(commonConfig, {
             statsOptions: null,
             logLevel: 'info'
         })
-    ],
-    devServer: {
-        devServer: {
-            contentBase: './dist'
-        },
-    }
+    ]
 });
