597a2f1fcaaecb159bae8f09f5f257e30c954a9f
徐登伟 <xudengwei1991@gmail.com>
2018-04-03 09:44:16 +0800

    modify the path of 'main.ts'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'src/app/app.component.html',
-    styleUrls: ['src/app/app.component.css']
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
     title = 'app';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
