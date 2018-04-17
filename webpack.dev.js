bedc734f4fe4b994c7c4d05985838bd26eaa9bcd
徐登伟 <1103579820@qq.com>
2018-04-17 13:18:14 +0800

    add plugin of 'style-loader'

--- webpack.dev.js
+++ webpack.dev.js
@@ -45,7 +45,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
+                loader: [ 'style-loader', 'css-loader' ]
             }
         ]
     },
