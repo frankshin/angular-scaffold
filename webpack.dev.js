c50c76489b5332ebfe0678c110cf0b831ba07e89
徐登伟 <1103579820@qq.com>
2018-04-17 14:43:34 +0800

    turn devServer's path to root path

--- webpack.dev.js
+++ webpack.dev.js
@@ -21,7 +21,7 @@ module.exports = {
         extensions: ['.ts', '.js']  // 自动补全识别后缀
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: './',
         hot: true,
         compress: true,
         inline: true,
@@ -79,7 +79,7 @@ module.exports = {
     ],
     output: {
         // 虽然我们告诉Webpack把输出包放到dist目录，但实际上开发服务器把这些包都放在了内存里，而不会把它们写到硬盘中。 所以在dist目录下是找不到任何文件的(至少现在这个开发环境下构建时没有)
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './'),
         filename: '[name].js',
         chunkFilename: '[id].chunk.js'
     }
